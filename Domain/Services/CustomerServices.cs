3ad445062c62d3667253d4a84b2abcce30ed611a
Susana Barahona <juliasbarahonamejia@gmail.com>
2026-06-13 21:39:02 -0600

    Agregar archivos de proyecto.

---
+++ Domain/Services/CustomerServices.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models.DTO;
+using Domain.Models.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Services
+{
+    public class CustomerServices
+    {
+        private readonly ICustomer _customer;//todo lo private llleva _(guion bajo)
+
+        public CustomerServices(ICustomer customer)
+        {
+
+            _customer = customer;
+
+        }
+        public async Task<List<CustomerDTO>> GetCustomers()
+        {
+            return await _customer.GetCustomers();
+        }
+        public async Task<CustomerDTO>GetCustomersById(string CustomerID)
+        {
+            return await _customer.GetCustomerById(CustomerID);
+        }
+    }
+}
---
+++ NorthwinAdmin/Program.cs
@@ -0,0 +1,33 @@
+using Data_access.Service;
+using Domain.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+
+namespace NorthwinAdmin
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            using IHost host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+            {
+                services.AddDataAccess(context.Configuration);
+                services.AddTransient<CustomerServices>();
+
+                services.AddTransient<Form1>();
+
+            }).Build();
+            var mainForm = host.Services.GetRequiredService<Form1>();
+            Application.Run(mainForm);
+        }
+    }
+}
