cabfa081c395e3bb2b95c2d21fa85c8e33a15bef
ekiara992-commits <ekiara992@gmail.com>
2026-07-05 18:35:26 -0600

    Reporte de ventas por empleados limpio

---
+++ Domain/Services/VentasEmpleadosServices.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Models.DTO;
+using Domain.Models.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Domain.Models.DTO;
+using Domain.Models.Intefaces;
+
+namespace Domain.Services
+{
+    public class VentasEmpleadosServices
+    {
+        private readonly IVentasEmpleados _ventasEmpleados;
+
+        public VentasEmpleadosServices(IVentasEmpleados ventasEmpleados)
+        {
+            _ventasEmpleados = ventasEmpleados;
+        }
+
+        public async Task<List<VentasEmpleadosDTO>> GetVentasEmpleados(DateTime fechaInicio, DateTime fechaFin)
+        {
+            return await _ventasEmpleados.GetVentasEmpleados(fechaInicio, fechaFin);
+        }
+    }
+}
