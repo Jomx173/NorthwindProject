3627364a07703fe23ec908dfe6b6ef4358951c49
Susana Barahona <juliasbarahonamejia@gmail.com>
2026-07-05 22:52:16 -0600

    Guardado temporal de archivos de empleados

---
+++ Domain/Models/DTO/OrderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models.DTO
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public int? EmployeeId { get; set; } // El "?" corrige el error CS0266 de la línea 70
+        public DateTime? OrderDate { get; set; }
+        public string ShipName { get; set; } // Añadido para corregir el error de la línea 72
+        public string ShipCity { get; set; }
+    }
+    }
