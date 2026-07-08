9cf8ac0c4efc5c318976aeca08b8b9992aaf43eb
Susana Barahona <juliasbarahonamejia@gmail.com>
2026-07-08 11:17:08 -0600

    Creacion del diseño del menú de gestiones

--- NorthwinAdmin/FrmMenuGestiones.Designer.cs
+++ NorthwinAdmin/FrmMenuGestiones.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            menuStrip1 = new MenuStrip();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenuGestiones));
             label1 = new Label();
             label8 = new Label();
             btnEmpleados = new Button();
@@ -36,17 +36,27 @@
             button3 = new Button();
             button4 = new Button();
             button5 = new Button();
-            button6 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            menuStrip1.ImageScalingSize = new Size(20, 20);
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(1083, 24);
-            menuStrip1.TabIndex = 0;
-            menuStrip1.Text = "menuStrip1";
             // 
             // label1
             // 
@@ -55,9 +65,9 @@
             label1.ForeColor = Color.FromArgb(0, 0, 64);
             label1.Location = new Point(30, 24);
             label1.Name = "label1";
-            label1.Size = new Size(285, 46);
+            label1.Size = new Size(325, 46);
             label1.TabIndex = 1;
-            label1.Text = "Panel de Control";
+            label1.Text = "Menú de Gestiones";
             // 
             // label8
             // 
@@ -72,89 +82,270 @@
             // 
             // btnEmpleados
             // 
-            btnEmpleados.BackColor = SystemColors.InactiveBorder;
+            btnEmpleados.BackColor = Color.White;
             btnEmpleados.Font = new Font("Nirmala UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnEmpleados.Location = new Point(42, 119);
             btnEmpleados.Name = "btnEmpleados";
             btnEmpleados.Size = new Size(315, 207);
             btnEmpleados.TabIndex = 3;
-            btnEmpleados.Text = "Empleados";
             btnEmpleados.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            button2.BackColor = SystemColors.InactiveBorder;
+            button2.BackColor = Color.White;
             button2.Location = new Point(380, 119);
             button2.Name = "button2";
             button2.Size = new Size(315, 207);
             button2.TabIndex = 4;
-            button2.Text = "button2";
             button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
-            button3.BackColor = SystemColors.InactiveBorder;
+            button3.BackColor = Color.White;
             button3.Location = new Point(719, 119);
             button3.Name = "button3";
             button3.Size = new Size(315, 207);
             button3.TabIndex = 5;
-            button3.Text = "button3";
             button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
-            button4.BackColor = SystemColors.InactiveBorder;
+            button4.BackColor = Color.White;
             button4.Location = new Point(42, 355);
             button4.Name = "button4";
             button4.Size = new Size(315, 207);
             button4.TabIndex = 6;
-            button4.Text = "button4";
             button4.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
-            button5.BackColor = SystemColors.InactiveBorder;
+            button5.BackColor = Color.White;
             button5.Location = new Point(380, 355);
             button5.Name = "button5";
             button5.Size = new Size(315, 207);
             button5.TabIndex = 7;
-            button5.Text = "button5";
             button5.UseVisualStyleBackColor = false;
             // 
-            // button6
+            // label2
             // 
-            button6.BackColor = SystemColors.InactiveBorder;
-            button6.Location = new Point(719, 355);
-            button6.Name = "button6";
-            button6.Size = new Size(315, 207);
-            button6.TabIndex = 8;
-            button6.Text = "button6";
-            button6.UseVisualStyleBackColor = false;
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.BackColor = Color.White;
+            label2.Font = new Font("Nirmala UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(130, 220);
+            label2.Name = "label2";
+            label2.Size = new Size(144, 39);
+            label2.TabIndex = 8;
+            label2.Text = "Empleados";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label3.BackColor = Color.White;
+            label3.Font = new Font("Nirmala UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(95, 259);
+            label3.Name = "label3";
+            label3.RightToLeft = RightToLeft.No;
+            label3.Size = new Size(209, 48);
+            label3.TabIndex = 9;
+            label3.Text = "Gestione la información de los empleados.";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ControlLightLight;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(119, 139);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(166, 78);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label4.BackColor = Color.White;
+            label4.Font = new Font("Nirmala UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(463, 220);
+            label4.Name = "label4";
+            label4.Size = new Size(144, 39);
+            label4.TabIndex = 11;
+            label4.Text = "Productos";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label5.BackColor = Color.White;
+            label5.Font = new Font("Nirmala UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(799, 220);
+            label5.Name = "label5";
+            label5.Size = new Size(144, 39);
+            label5.TabIndex = 12;
+            label5.Text = "Pedidos";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label6.BackColor = Color.White;
+            label6.Font = new Font("Nirmala UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(119, 460);
+            label6.Name = "label6";
+            label6.Size = new Size(155, 39);
+            label6.TabIndex = 13;
+            label6.Text = "Proveedores";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label7.BackColor = Color.White;
+            label7.Font = new Font("Nirmala UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(463, 460);
+            label7.Name = "label7";
+            label7.Size = new Size(144, 39);
+            label7.TabIndex = 14;
+            label7.Text = "Categorías";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            label9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label9.BackColor = Color.White;
+            label9.Font = new Font("Nirmala UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(431, 259);
+            label9.Name = "label9";
+            label9.RightToLeft = RightToLeft.No;
+            label9.Size = new Size(209, 48);
+            label9.TabIndex = 15;
+            label9.Text = "Administra productos y existencias.";
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            label10.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label10.BackColor = Color.White;
+            label10.Font = new Font("Nirmala UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(774, 250);
+            label10.Name = "label10";
+            label10.RightToLeft = RightToLeft.No;
+            label10.Size = new Size(209, 48);
+            label10.TabIndex = 16;
+            label10.Text = "Gestiona órdenes y envíos.";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            label11.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label11.BackColor = Color.White;
+            label11.Font = new Font("Nirmala UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(95, 489);
+            label11.Name = "label11";
+            label11.RightToLeft = RightToLeft.No;
+            label11.Size = new Size(209, 48);
+            label11.TabIndex = 17;
+            label11.Text = "Controla alianzas y contactos.";
+            label11.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            label12.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label12.BackColor = Color.White;
+            label12.Font = new Font("Nirmala UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.Location = new Point(431, 489);
+            label12.Name = "label12";
+            label12.RightToLeft = RightToLeft.No;
+            label12.Size = new Size(209, 48);
+            label12.TabIndex = 18;
+            label12.Text = "Organiza productos por tipo.";
+            label12.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.ControlLightLight;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(450, 139);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(166, 78);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 19;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = SystemColors.ControlLightLight;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(787, 139);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(166, 78);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 20;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = SystemColors.ControlLightLight;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(119, 379);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(166, 78);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 21;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = SystemColors.ControlLightLight;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(450, 379);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(166, 78);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 22;
+            pictureBox5.TabStop = false;
             // 
             // FrmMenuGestiones
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1083, 596);
-            Controls.Add(button6);
+            BackColor = Color.GhostWhite;
+            ClientSize = new Size(1064, 596);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(pictureBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(btnEmpleados);
             Controls.Add(label1);
-            Controls.Add(menuStrip1);
             Controls.Add(label8);
-            MainMenuStrip = menuStrip1;
             Name = "FrmMenuGestiones";
-            Text = "FrmMenuGestiones";
+            Text = "Gestiones";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private MenuStrip menuStrip1;
         private Label label1;
         private Label label8;
         private Button btnEmpleados;
@@ -162,6 +353,20 @@
         private Button button3;
         private Button button4;
         private Button button5;
-        private Button button6;
+        private Label label2;
+        private Label label3;
+        private PictureBox pictureBox1;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
     }
 }
